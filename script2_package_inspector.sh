1ac7bffa16f220084a7511f4244b6f03aac49ef2
Sanjay-PT <sanjaypt1026@gmail.com>
2026-03-29 23:36:39 +0530

    Update script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
+# Script 2: Package Inspector
+# Author: SANJAY P T
+# Purpose: Check if a package is installed and display details
 
-PACKAGE="linux"
+PACKAGE="linux"   # Package name to check
 
+# Check installation using dpkg
 if dpkg -l | grep -q $PACKAGE; then
     echo "$PACKAGE is installed."
+
+    # Show package information
     dpkg -l | grep $PACKAGE
 else
     echo "$PACKAGE is NOT installed."
 fi
 
+# Provide description based on package name
 case $PACKAGE in
-    linux) echo "Linux Kernel: core of operating systems" ;;
-    apache2) echo "Apache: web server powering internet" ;;
-    mysql) echo "MySQL: open source database" ;;
-    firefox) echo "Firefox: open web browser" ;;
+    linux)
+        echo "Linux Kernel: Core component managing system resources"
+        ;;
+    apache2)
+        echo "Apache: Open-source web server"
+        ;;
+    mysql)
+        echo "MySQL: Database management system"
+        ;;
+    firefox)
+        echo "Firefox: Open-source web browser"
+        ;;
+    *)
+        echo "Unknown package"
+        ;;
 esac
