d68061763d11e13c26f954bae42053423e186cdb
Sanjay-PT <sanjaypt1026@gmail.com>
2026-03-29 23:04:51 +0530

    Create script4_log_analyzer.sh

---
+++ script4_log_analyzer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LOGFILE=$1
+KEYWORD=${2:-"error"}
+COUNT=0
+
+if [ ! -f "$LOGFILE" ]; then
+    echo "File not found"
+    exit 1
+fi
+
+while read LINE; do
+    echo "$LINE" | grep -iq "$KEYWORD"
+    if [ $? -eq 0 ]; then
+        COUNT=$((COUNT+1))
+    fi
+done < "$LOGFILE"
+
+echo "$KEYWORD found $COUNT times"
+
+grep -i "$KEYWORD" "$LOGFILE" | tail -5
