163631cfad1ad23dd56d48dba3305e7514f148ae
Sanjay-PT <sanjaypt1026@gmail.com>
2026-03-29 23:38:19 +0530

    Update script5_manifesto_generator.sh

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+# Script 5: Open Source Manifesto Generator
+# Author: SANJAY P T
+# Purpose: Generate a personalized open-source statement
 
-read -p "Tool: " TOOL
-read -p "Freedom means: " FREEDOM
-read -p "Build: " BUILD
+echo "Answer the following questions:"
+echo ""
 
-DATE=$(date)
-OUTPUT="manifesto.txt"
+# Take user inputs
+read -p "Tool you use daily: " TOOL
+read -p "Meaning of freedom: " FREEDOM
+read -p "What will you build: " BUILD
 
+# Generate file details
+DATE=$(date)                      # Current date
+OUTPUT="manifesto.txt"            # Output file
+
+# Write responses into file
 echo "On $DATE, I believe in open source." > $OUTPUT
-echo "Using $TOOL, I define freedom as $FREEDOM." >> $OUTPUT
-echo "I will build $BUILD and share it with the world." >> $OUTPUT
+echo "Using $TOOL, freedom means $FREEDOM." >> $OUTPUT
+echo "I will build $BUILD and share it openly." >> $OUTPUT
 
+# Show output
+echo "Manifesto saved to $OUTPUT"
 cat $OUTPUT
