755f5b6d019ebd8feb6841311614fda29cf02c75
Sanjay-PT <sanjaypt1026@gmail.com>
2026-03-29 23:37:49 +0530

    Update script3_disk_auditor.sh

--- script3_disk_auditor.sh
+++ script3_disk_auditor.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+# Script 3: Disk and Permission Auditor
+# Author: SANJAY P T
+# Purpose: Show directory size and permissions
 
+# List of important system directories
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
 echo "Directory Audit Report"
+echo "----------------------"
 
+# Loop through each directory
 for DIR in "${DIRS[@]}"; do
     if [ -d "$DIR" ]; then
+        
+        # Get permissions, owner and group
         PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        
+        # Get directory size
         SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
-        echo "$DIR => $PERMS | Size: $SIZE"
+        
+        # Display result
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR not found"
     fi
 done
 
-CONFIG="/boot"
-if [ -d "$CONFIG" ]; then
-    echo "Kernel config exists: $(ls -ld $CONFIG)"
+# Check Linux kernel related directory
+if [ -d "/boot" ]; then
+    echo "Kernel directory exists:"
+    ls -ld /boot
 fi
